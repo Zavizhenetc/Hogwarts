dde0bddbd41f2a75932bd6d3bccd97e27b932459
Alex Zav <aazavizhenetc@scid.ru>
2021-11-09 15:26:55 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <transition name="fade" mode="out-in">
+      <router-view/>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+
+  }
+}
+</script>
+<style lang="scss">
+body {
+  padding: 0;
+  margin: 0;
+
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  min-height: 100vh;
+  padding: 0;
+  margin: 0;
+  background-color: var(--background);
+  cursor: var(--cursor);
+}
+
+:root {
+  --color-text: #ffffff;
+  --background: #1F2123;
+  --cursor: url('./assets/images/cursor_harry.png'), auto;
+}
+
+.fade-enter-active {
+  transition: transform .6s, opacity .8s ease-in;
+}
+
+.fade-leave-active {
+  transition: transform .6s, opacity .8s ease-out;
+}
+
+.fade-enter {
+  opacity: .05;
+  transform: translate(100%)
+}
+
+.fade-leave-to {
+  opacity: .05;
+  transform: translate(-100%)
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,65 @@
+<template>
+
+  <div class="header">
+    <div class="header__nav">
+      <router-link to="/" class="header__btn"><p>Home</p></router-link>
+      <router-link to="/about" class="header__btn"><p>Hogwarts staff</p></router-link>
+      <router-link to="/about" class="header__btn"><p>Hogwarts students</p></router-link>
+    </div>
+
+    <div class="header__weather">
+      Здесь будет погода
+    </div>
+  </div>
+
+
+</template>
+<script>
+
+export default {
+  name: 'Header',
+  data() {
+    return {}
+  },
+  methods: {},
+
+  mounted() {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+@import '../assets/styles/vars';
+
+.header {
+  @extend %font;
+  display: flex;
+  min-height: 90px;
+  max-width: 100%;
+  padding: 1%;
+  align-items: center;
+  justify-content: space-between;
+  color: var(--color-text);
+
+  &__nav {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+  }
+
+  &__btn {
+    color: var(--color-text);
+  }
+
+  &__weather {
+    font-size: 28px;
+    flex-grow: 0;
+    flex-shrink: 0;
+    margin-left: 28px;
+  }
+}
+
+
+</style>
